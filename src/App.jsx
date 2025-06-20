39ffc4a9398348f3754117f8e6e94939f056388c
Randy Parra <randyparra@hotmail.com>
2025-06-20 19:39:41 -0400

    Uncommented the Login forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
 import { useState } from 'react'
-import MainForm from './components/MainForm/MainForm'
-import MainForm2 from './components/MainForm2/MainForm2'
+// import MainForm from './components/MainForm/MainForm'
+// import MainForm2 from './components/MainForm2/MainForm2'
 
-// import './App.css'
+import './App.css'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
-// import 'bulma/css/bulma.css';
+import 'bulma/css/bulma.css';
 // import MediumForm from './components/MediumForm/MediumForm';
 // import SuperForm from './components/SuperForm/SuperForm';
-// import LoginForm from './components/LoginForm/LoginForm';
-// import Dashboard from './components/Dashboard/Dashboard';
+import LoginForm from './components/LoginForm/LoginForm';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
-  // const [count, setCount] = useState(0);
-  // const [isLoggedIn, setLoggedIn] = useState(false);
-  // let content = <></>
+  const [count, setCount] = useState(0);
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  let content = <></>
 
-  // function ChangeLoginMode() {
-  //   setLoggedIn( !isLoggedIn );
-  // }
+  function ChangeLoginMode() {
+    setLoggedIn( !isLoggedIn );
+  }
 
-  // if ( isLoggedIn == false ) {
-  //   content = <LoginForm isLoggedIn={isLoggedIn} />
-  // }
-  // else {
-  //   content = <Dashboard />
-  // }
+  if ( isLoggedIn == false ) {
+    content = <LoginForm isLoggedIn={isLoggedIn} />
+  }
+  else {
+    content = <Dashboard />
+  }
 
-  // return (
-  //   <>
-  //     { isLoggedIn
-  //     ? <Dashboard ChangeLoginMode={ChangeLoginMode} />
-  //     : <LoginForm ChangeLoginMode={ChangeLoginMode} /> }
-  //   </>
-  // )
   return (
-    <MainForm2 />
+    <>
+      { isLoggedIn
+      ? <Dashboard ChangeLoginMode={ChangeLoginMode} />
+      : <LoginForm ChangeLoginMode={ChangeLoginMode} /> }
+    </>
   )
+  // return (
+  //   <MainForm2 />
+  // )
 }
 
 export default App
